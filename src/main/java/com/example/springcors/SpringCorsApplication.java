bed38124d366ae4d532101db155f84517f8665ed
lgong2000 <lgong2000@gmail.com>
2024-05-03 11:25:11 -0500

    Initial Commit

---
+++ src/main/java/com/example/springcors/SpringCorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcors/SpringCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
